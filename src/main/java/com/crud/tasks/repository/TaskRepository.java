34f0e4c33d5e1d7c1150c16fa93564e97c765229
pflaz <pf@szach.net>
2018-04-12 06:26:58 +0200

    TaskController's methods added

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -4,10 +4,16 @@ import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepository extends CrudRepository<Task, Integer> {
     @Override
     List<Task> findAll();
 
-    Task findById(Long id);
+    @Override
+    Task save(Task task);
+
+    Optional<Task> findById(Long id);
+
+    void deleteById(Long id);
 }
